74073f15d3c69d7bde3198602e8ef71257712258
David Dunson <david.dunson@i3-corps.com>
2023-08-18 10:27:39 -0500

    Added asp.net core identity reference to identity server project.

---
+++ IdentityServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ApplicationDbContext.cs" company="Sample Company">
+//   © 2023 Sample Company.  All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace IdentityServer.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -6,7 +6,10 @@
 
 namespace IdentityServer
 {
+    using IdentityServer.Data;
     using IdentityServer.Services;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
 
     public class Program
     {
@@ -14,7 +17,46 @@ namespace IdentityServer
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddIdentityServer()
+            var config = builder.Configuration;
+            var connectionString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => { options.SignIn.RequireConfirmedAccount = true; })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    var google = config.GetSection("Authentication:Google");
+                    options.ClientId = google["ClientId"];
+                    options.ClientSecret = google["ClientSecret"];
+                })
+                .AddFacebook(options =>
+                {
+                    var facebook = config.GetSection("Authentication:Facebook");
+                    options.ClientId = facebook["ClientId"];
+                    options.ClientSecret = facebook["ClientSecret"];
+                })
+                .AddMicrosoftAccount(options =>
+                {
+                    var microsoft = config.GetSection("Authentication:Microsoft");
+                    options.ClientId = microsoft["ClientId"];
+                    options.ClientSecret = microsoft["ClientSecret"];
+                })
+                .AddTwitter(options =>
+                {
+                    var twitter = config.GetSection("Authentication:Twitter");
+                    options.ConsumerKey = twitter["ConsumerKey"];
+                    options.ConsumerSecret = twitter["ConsumerSecret"];
+                    options.RetrieveUserDetails = true;
+                });
+
+            builder.Services.AddIdentityServer(options =>
+                {
+                    options.UserInteraction.LoginUrl = "/Identity/Account/Login";
+                    options.UserInteraction.LogoutUrl = "/Identity/Account/Logout";
+                })
                 .AddSigningCredential(builder.Configuration)
                 .AddClientStore<ClientStore>()
                 .AddResourceStore<ResourceStore>()
